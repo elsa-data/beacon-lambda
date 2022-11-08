98ff25d645ce829a7aee5839150730daa04a68a7
Marko Malenic <mmalenic1@gmail.com>
2022-11-08 15:47:20 +1100

    Simplify tracing subscriber error

--- application/lambda/beacon/src/main.rs
+++ application/lambda/beacon/src/main.rs
@@ -1,7 +1,4 @@
-use std::io::ErrorKind;
-
 use lambda_runtime::{service_fn, Error};
-use tokio::io;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::{fmt, EnvFilter, Registry};
 
@@ -13,12 +10,8 @@ async fn main() -> Result<(), Error> {
     let fmt_layer = fmt::Layer::default();
     let subscriber = Registry::default().with(env_filter).with(fmt_layer);
 
-    tracing::subscriber::set_global_default(subscriber).map_err(|err| {
-        io::Error::new(
-            ErrorKind::Other,
-            format!("failed to install `tracing` subscriber: {}", err),
-        )
-    })?;
+    tracing::subscriber::set_global_default(subscriber)
+        .map_err(|err| Error::from(format!("failed to install `tracing` subscriber: {}", err)))?;
 
     let handler = service_fn(beacon_handler);
     lambda_runtime::run(handler).await?;
